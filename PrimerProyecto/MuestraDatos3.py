7134e9371cb3cf493107ab16f4ec09a4c6a8512d
Gama <88944546+Code0008@users.noreply.github.com>
2022-05-03 19:32:32 -0500

    Primer Proyecto
    En esta parte del repositorio mostraremos las diversas formas de manipular datos en nuestro primer proyecto real

---
+++ PrimerProyecto/MuestraDatos3.py
@@ -0,0 +1,7 @@
+print(50 * '*')
+print(25 * ' ' + "Buenas")
+print(50 * '*')
+
+nombreDeUsuario = input("Hola! quieres darme tu nombre? \n[>] " )
+saludoUsuario = "Hola que tal {}".format(nombreDeUsuario)
+print(saludoUsuario)
---
+++ PrimerProyecto/MuestraDatos4.py
@@ -0,0 +1,8 @@
+print(50 * '*')
+print(25 * ' ' + "Buenas")
+print(50 * '*')
+
+nombreDeUsuario = input("Hola! quieres darme tu nombre? \n[>] " )
+saludoUsuario = f"Hola que tal {nombreDeUsuario} "
+print(saludoUsuario)
+
